6128ebaeee50a506d5c4708cf3321028a4280345
weenchvd <d88744@gmail.com>
2021-01-18 16:12:25 +0300

    Computing FOLLOW Sets for Non-Terminal Symbols. Refactoring grammar.

--- Grammar/grammar.cpp
+++ Grammar/grammar.cpp
@@ -29,7 +29,7 @@ int main()
 		else if (ifs.fail()) std::cout << "ifs.fail() is true" << std::endl;
 		else std::cout << "ifs.bad() is true" << std::endl;
 	}
-	grammar.RemoveDirectAndIndirectLeftRecursion(std::cerr);
+	grammar.TransformGrammar(std::cerr);
 	ofs << grammar;
 
 	return 0;
@@ -37,110 +37,14 @@ int main()
 
 namespace CFG
 {
-	LexemeID BNFGrammar::GetLexemeID(const std::string& lexeme)
+	void BNFGrammar::Print(std::ostream& os, const std::vector<LexemeID>& set) const
 	{
-		auto result = id.emplace(lexeme, next);
-		if (result.second == true) {
-			lex.push_back(lexeme);
-			++next;
-		}
-		return result.first->second;
-	}
-
-	void BNFGrammar::RemoveDirectAndIndirectLeftRecursion(std::ostream& error)
-	{
-		if (!CheckGrammar(error)) return;
-		FindAllNonterminals(error);
-		RemoveRecursion(error);
-	}
-
-	void BNFGrammar::FindAllNonterminals(std::ostream& error)
-	{
-		typeList.resize(next);
-		for (LexemeType& lex : typeList) {
-			lex = LexemeType::T;
-		}
-		for (const Production& p : prodList) {
-			typeList[p.nt] = LexemeType::NT;
+		for (LexemeID id : set) {
+			os << lex[id] << ' ';
 		}
 	}
 
-	// CHAPTER 3 Parsers, 3.3 TOP-DOWN PARSING, Part "Eliminating Left Recursion"
-	void BNFGrammar::RemoveRecursion(std::ostream& error)
-	{
-		std::vector<Production> newProdList;
-		for (int i = 0; i < prodList.size(); ++i) {
-			Production& curProd = prodList[i];
-			// convert each indirect left recursion into a direct left recursion
-			for (int j = 0; j < i; ++j) {
-				std::vector<Derived> newDerList;
-				for (const Derived& d : curProd.derList) {
-					if (prodList[j].nt == d[0]) {
-						int index = -1;
-						for (int k = 0; k < newProdList.size(); ++k) {
-							if (newProdList[k].nt == d[0]) {
-								index = k;
-								break;
-							}
-						}
-						for (const Derived& dd : newProdList[index].derList) {
-							newDerList.push_back(dd);
-							const auto it = newDerList[newDerList.size() - 1].end();
-							newDerList[newDerList.size() - 1].insert(it, ++d.begin(), d.end());
-						}
-					}
-					else {
-						newDerList.push_back(d);
-					}
-				}
-				curProd.derList = newDerList;
-//error << "--AFTER INDIRECT i = " << i << ", j = " << j << ":" << std::endl;
-//error << "-----PRODLIST:" << std::endl;
-//for (const Production& p : prodList) error << p;
-//error << "-----NEWPRODLIST:" << std::endl;
-//for (const Production& p : newProdList) error << p;
-//error << "----------------------------------------" << std::endl;
-			}
-			// remove any direct left recursion
-			bool leftRecursion = false;
-			for (const Derived& d : curProd.derList) {
-				if (curProd.nt == d[0]) {
-					leftRecursion = true;
-					break;
-				}
-			}
-			if (leftRecursion) {
-				Production replacedProd{ curProd.nt };
-				Production newProd{ GetLexemeID(lex[curProd.nt] + suffNT) };
-				for (const Derived& d : curProd.derList) {
-					if (curProd.nt == d[0]) {
-						Derived newDer{ ++d.begin(), d.end() };
-						newDer.push_back(newProd.nt);
-						newProd.derList.push_back(newDer);
-					}
-					else {
-						replacedProd.derList.push_back(d);
-						replacedProd.derList[replacedProd.derList.size() - 1].push_back(newProd.nt);
-					}
-				}
-				newProd.derList.push_back(Derived{ {GetLexemeID(lexES)} });
-				newProdList.push_back(replacedProd);
-				newProdList.push_back(newProd);
-			}
-			else {
-				newProdList.push_back(curProd);
-			}
-//error << "``AFTER DIRECT i = " << i << ":" << std::endl;
-//error << "`````PRODLIST:" << std::endl;
-//for (const Production& p : prodList) error << p;
-//error << "`````NEWPRODLIST:" << std::endl;
-//for (const Production& p : newProdList) error << p;
-//error << "````````````````````````````````````````" << std::endl;
-		}
-		prodList = newProdList;
-	}
-
-	bool BNFGrammar::CheckGrammar(std::ostream& error) const
+	bool BNFGrammar::CheckOriginalGrammar(std::ostream& error) const
 	{
 		std::vector<int> counter(next);
 		for (int& i : counter) {
@@ -151,10 +55,10 @@ namespace CFG
 		if (iter == id.end()) {
 			lexESisPresent = false;
 		}
-		for (int i = 0; i < prodList.size(); ++i) {
-			const Production& p = prodList[i];
+		for (int i = 0; i < prodSet.size(); ++i) {
+			const Production& p = prodSet[i];
 			++counter[p.nt];
-			for (const Derived& d : p.derList) {
+			for (const Derived& d : p.derSet) {
 				if (d.size() == 1 && d[0] == p.nt) {
 					error << "\tError. Grammar has cycle '" << lex[p.nt] << " -> " << lex[p.nt] << "'" << std::endl;
 					return false;
@@ -164,9 +68,9 @@ namespace CFG
 					return false;
 				}
 				for (int j = 0; j < i; ++j) {
-					if (d.size() == 1 && d[0] == prodList[j].nt) {
-						error << "\tWarning. Probably the grammar has indirect cycle '" << lex[prodList[j].nt]
-							<< " -> " << lex[prodList[i].nt] << " -> " << lex[prodList[j].nt] << "'" << std::endl;
+					if (d.size() == 1 && d[0] == prodSet[j].nt) {
+						error << "\tWarning. Probably the grammar has indirect cycle '" << lex[prodSet[j].nt]
+							<< " -> " << lex[prodSet[i].nt] << " -> " << lex[prodSet[j].nt] << "'" << std::endl;
 					}
 				}
 			}
@@ -178,6 +82,257 @@ namespace CFG
 			}
 		}
 		return true;
+	}
+
+	bool BNFGrammar::SetContainsOnlyTerminalsAndNonterminals(const Derived& derived) const
+	{
+		for (LexemeID id : derived) {
+			switch (typeSet[id]) {
+			case LexemeType::T:
+			case LexemeType::NT:
+				break;
+			default:
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool BNFGrammar::SetContains(const std::vector<LexemeID>& set, LexemeID target) const
+	{
+		for (LexemeID id : set) {
+			if (id == target) return true;
+		}
+		return false;
+	}
+
+	First BNFGrammar::GetFirstSetForProduction(size_t productionIndex, size_t derivedIndex) const
+	{
+		First set;
+		for (LexemeID id : prodSet[productionIndex].derSet[derivedIndex]) {
+			Merge(set, firstSet[id]);
+			if (!SetContains(firstSet[id], idES)) {
+				break;
+			}
+		}
+		return set;
+	}
+
+	// add SOURCE content to RECEIVER (only adds unique elements),
+	// returns the number of elements added
+	int BNFGrammar::Merge(std::vector<LexemeID>& receiver, const std::vector<LexemeID>& source) const
+	{
+		int num = 0;
+		for (LexemeID id : source) {
+			if (!SetContains(receiver, id)) {
+				receiver.push_back(id);
+				++num;
+			}
+		}
+		return num;
+	}
+
+	// add SOURCE content to RECEIVER (only adds unique elements, but other than EXCEPTION),
+	// returns the number of elements added
+	int BNFGrammar::MergeExcept(std::vector<LexemeID>& receiver, const std::vector<LexemeID>& source,
+		LexemeID exception) const
+	{
+		int num = 0;
+		for (LexemeID id : source) {
+			if (id != exception && !SetContains(receiver, id)) {
+				receiver.push_back(id);
+				++num;
+			}
+		}
+		return num;
+	}
+
+	LexemeID BNFGrammar::GetLexemeID(const std::string& lexeme)
+	{
+		auto result = id.emplace(lexeme, next);
+		if (result.second == true) {
+			lex.push_back(lexeme);
+			++next;
+		}
+		return result.first->second;
+	}
+
+	void BNFGrammar::TransformGrammar(std::ostream& error)
+	{
+		RemoveDirectAndIndirectLeftRecursion(error);
+		FindAllNonterminals(error);
+		FindAllFirstSets(error);
+		FindAllFollowSets(error);
+	}
+
+	void BNFGrammar::RemoveDirectAndIndirectLeftRecursion(std::ostream& error)
+	{
+		if (!CheckOriginalGrammar(error)) return;
+		RemoveRecursion(error);
+	}
+
+	// CHAPTER 3 Parsers, 3.3 TOP-DOWN PARSING, Part "Eliminating Left Recursion",
+	// FIGURE 3.6 Removal of Indirect Left Recursion
+	void BNFGrammar::RemoveRecursion(std::ostream& error)
+	{
+		std::vector<Production> newProdList;
+		for (int i = 0; i < prodSet.size(); ++i) {
+			Production& curProd = prodSet[i];
+			// convert each indirect left recursion into a direct left recursion
+			for (int j = 0; j < i; ++j) {
+				std::vector<Derived> newDerList;
+				for (const Derived& d : curProd.derSet) {
+					if (prodSet[j].nt == d[0]) {
+						int index = -1;
+						for (int k = 0; k < newProdList.size(); ++k) {
+							if (newProdList[k].nt == d[0]) {
+								index = k;
+								break;
+							}
+						}
+						for (const Derived& dd : newProdList[index].derSet) {
+							newDerList.push_back(dd);
+							const auto it = newDerList[newDerList.size() - 1].end();
+							newDerList[newDerList.size() - 1].insert(it, ++d.begin(), d.end());
+						}
+					}
+					else {
+						newDerList.push_back(d);
+					}
+				}
+				curProd.derSet = newDerList;
+//error << "--AFTER INDIRECT i = " << i << ", j = " << j << ":" << std::endl;
+//error << "-----PRODLIST:" << std::endl;
+//for (const Production& p : prodSet) error << p;
+//error << "-----NEWPRODLIST:" << std::endl;
+//for (const Production& p : newProdList) error << p;
+//error << "----------------------------------------" << std::endl;
+			}
+			// remove any direct left recursion
+			bool leftRecursion = false;
+			for (const Derived& d : curProd.derSet) {
+				if (curProd.nt == d[0]) {
+					leftRecursion = true;
+					break;
+				}
+			}
+			if (leftRecursion) {
+				Production replacedProd{ curProd.nt };
+				Production newProd{ GetLexemeID(lex[curProd.nt] + suffNT) };
+				for (const Derived& d : curProd.derSet) {
+					if (curProd.nt == d[0]) {
+						Derived newDer{ ++d.begin(), d.end() };
+						newDer.push_back(newProd.nt);
+						newProd.derSet.push_back(newDer);
+					}
+					else {
+						replacedProd.derSet.push_back(d);
+						replacedProd.derSet[replacedProd.derSet.size() - 1].push_back(newProd.nt);
+					}
+				}
+				newProd.derSet.push_back(Derived{ {idES} });
+				newProdList.push_back(replacedProd);
+				newProdList.push_back(newProd);
+			}
+			else {
+				newProdList.push_back(curProd);
+			}
+//error << "``AFTER DIRECT i = " << i << ":" << std::endl;
+//error << "`````PRODLIST:" << std::endl;
+//for (const Production& p : prodSet) error << p;
+//error << "`````NEWPRODLIST:" << std::endl;
+//for (const Production& p : newProdList) error << p;
+//error << "````````````````````````````````````````" << std::endl;
+		}
+		prodSet = newProdList;
+	}
+
+	void BNFGrammar::FindAllNonterminals(std::ostream& error)
+	{
+		typeSet.resize(next);
+		for (LexemeType& lex : typeSet) {
+			lex = LexemeType::T;
+		}
+		for (const Production& p : prodSet) {
+			typeSet[p.nt] = LexemeType::NT;
+		}
+		typeSet[idES] = LexemeType::EMPTYSTRING;
+		typeSet[idEOF] = LexemeType::ENDOFFILE;
+	}
+
+	// CHAPTER 3 Parsers, 3.3 TOP-DOWN PARSING, Part "Backtrack-Free Parsing",
+	// FIGURE 3.7 Computing FIRST Sets for Symbols in a Grammar
+	void BNFGrammar::FindAllFirstSets(std::ostream& error)
+	{
+		firstSet.resize(next);
+		for (LexemeID id = 0; id < next; ++id) {
+			switch (typeSet[id]) {
+			case LexemeType::EMPTYSTRING:
+			case LexemeType::ENDOFFILE:
+			case LexemeType::T:
+				firstSet[id].push_back(id);
+				break;
+			}
+		}
+		bool setsAreChanging = true;
+		while (setsAreChanging) {
+			setsAreChanging = false;
+			for (const Production& p : prodSet) {
+				for (const Derived& d : p.derSet) {
+					std::vector<LexemeID> rhs;
+					int i = 0;
+					int k = d.size() - 1;
+					if (SetContainsOnlyTerminalsAndNonterminals(d)) {
+						MergeExcept(rhs, firstSet[d[i]], idES);
+						while (SetContains(firstSet[d[i]], idES) && i < k) {
+							MergeExcept(rhs, firstSet[d[++i]], idES);
+						}
+					}
+					if (i == k && SetContains(firstSet[d[k]], idES)) {
+						rhs.push_back(idES);
+					}
+					if (Merge(firstSet[p.nt], rhs) > 0) {
+						setsAreChanging = true;
+					}
+				}
+			}
+		}
+	}
+
+	// CHAPTER 3 Parsers, 3.3 TOP-DOWN PARSING, Part "Backtrack-Free Parsing",
+	// FIGURE 3.8 Computing FOLLOW Sets for Non-Terminal Symbols
+	void BNFGrammar::FindAllFollowSets(std::ostream& error)
+	{
+		followSet.resize(next);
+		bool setsAreChanging = true;
+		while (setsAreChanging) {
+			setsAreChanging = false;
+			for (const Production& p : prodSet) {
+				for (const Derived& d : p.derSet) {
+					std::vector<LexemeID> trailer = followSet[p.nt];
+					for (int i = d.size() - 1; i >= 0; --i) {
+						LexemeID id = d[i];
+						if (typeSet[id] == LexemeType::NT) {
+							if (Merge(followSet[id], trailer) > 0) {
+								setsAreChanging = true;
+							}
+							if (SetContains(firstSet[id], idES)) {
+								MergeExcept(trailer, firstSet[id], idES);
+							}
+							else {
+								trailer = firstSet[id];
+							}
+						}
+						else {
+							trailer = firstSet[id];
+						}
+					}
+				}
+			}
+		}
+		for (const Production& p : prodSet) {
+			followSet[p.nt].push_back(idEOF);
+		}
 	}
 
 	std::istream& operator>>(std::istream& is, Production& prod)
@@ -227,7 +382,7 @@ namespace CFG
 				is.setstate(std::ios_base::failbit);
 				return is;
 			}
-			temp.derList.push_back(derived);
+			temp.derSet.push_back(derived);
 		}
 		prod = temp;
 		return is;
@@ -240,8 +395,10 @@ namespace CFG
 		getline(is, secondLine);
 		if (!is) return is;
 		std::istringstream iss{ secondLine };
-		iss >> gram.prefNT >> gram.prefDer >> gram.suffNT >> gram.lexES;
+		iss >> gram.prefNT >> gram.prefDer >> gram.suffNT >> gram.lexES >> gram.lexEOF;
 		if (!iss) return is;
+		gram.idES = gram.GetLexemeID(gram.lexES);
+		gram.idEOF = gram.GetLexemeID(gram.lexEOF);
 		Production prod;
 		while (is) {
 			is >> prod;
@@ -256,7 +413,7 @@ namespace CFG
 		constexpr int spaceCount = 2;
 		// output left part (NT) of production
 		os << BNFGrammar::prefNT << ' ' << prod.gram->lex[prod.nt] << ' ';
-		for (size_t j = 0; j < prod.derList.size(); ++j) {
+		for (size_t j = 0; j < prod.derSet.size(); ++j) {
 			// output right part of production
 			if (j == 0) {
 				os << BNFGrammar::prefDer;
@@ -266,7 +423,7 @@ namespace CFG
 					+ BNFGrammar::prefDer.size();
 				os << std::setw(indent) << std::right << BNFGrammar::prefDer;
 			}
-			const Derived& derived = prod.derList[j];
+			const Derived& derived = prod.derSet[j];
 			for (size_t k = 0; k < derived.size(); ++k) {
 				os << ' ' << prod.gram->lex[derived[k]];
 			}
@@ -291,13 +448,13 @@ namespace CFG
 		os << gram.comment << std::endl;
 		// output prefixes
 		os << gram.prefNT << ' ' << gram.prefDer << ' '
-			<< gram.suffNT << ' ' << gram.lexES << std::endl;
+			<< gram.suffNT << ' ' << gram.lexES << ' ' << gram.lexEOF << std::endl;
 		// output all productions
-		for (size_t i = 0; i < gram.prodList.size(); ++i) {
-			const Production& prod = gram.prodList[i];
+		for (size_t i = 0; i < gram.prodSet.size(); ++i) {
+			const Production& prod = gram.prodSet[i];
 			// output left part (NT) of production
 			os << gram.prefNT << ' ' << std::setw(lexMaxLength) << std::left << gram.lex[prod.nt] << ' ';
-			for (size_t j = 0; j < prod.derList.size(); ++j) {
+			for (size_t j = 0; j < prod.derSet.size(); ++j) {
 				// output right part of production
 				if (j == 0) {
 					os << std::setw(indent1) << std::right << gram.prefDer;
@@ -305,10 +462,26 @@ namespace CFG
 				else {
 					os << std::setw(indent2) << std::right << gram.prefDer;
 				}
-				const Derived& derived = prod.derList[j];
+				const Derived& derived = prod.derSet[j];
 				for (size_t k = 0; k < derived.size(); ++k) {
 					os << ' ' << gram.lex[derived[k]];
 				}
+				os << std::endl;
+			}
+		}
+		// output FIRST sets
+		os << std::endl << std::endl;
+		for (int i = 0; i < gram.firstSet.size(); ++i) {
+			os << "FIRST(" << gram.lex[i] << "): ";
+			gram.Print(os, gram.firstSet[i]);
+			os << std::endl;
+		}
+		// output FOLLOW sets
+		os << std::endl << std::endl;
+		for (int i = 0; i < gram.followSet.size(); ++i) {
+			if (gram.followSet[i].size() > 0) {
+				os << "FOLLOW(" << gram.lex[i] << "): ";
+				gram.Print(os, gram.followSet[i]);
 				os << std::endl;
 			}
 		}
--- Grammar/grammar.hpp
+++ Grammar/grammar.hpp
@@ -10,22 +10,24 @@ namespace CFG								// context-free grammar
 
 	using LexemeID = size_t;									// lexeme ID
 	using Derived = std::vector<LexemeID>;
+	using First = std::vector<LexemeID>;						// FIRST set
+	using Follow = std::vector<LexemeID>;						// FOLLOW set
 
 	constexpr int derivedShift = 10;
 
 	enum class LexemeType {
-		T, NT
+		EMPTYSTRING, ENDOFFILE, T, NT
 	};
 
-	enum class TType {											// T type
-		EMPTYSTRING, END
-	};
+	//enum class TType {											// T type
+	//	EMPTYSTRING, END
+	//};
 
 	class BNFGrammar;
 
 	struct Production {
 		LexemeID nt;											// NT
-		std::vector<Derived> derList;							// list of T and/or NT derived from the 'nt'
+		std::vector<Derived> derSet;							// list of T and/or NT derived from the 'nt'
 		static BNFGrammar* gram;
 
 		Production() : nt{ 0 } {}
@@ -35,41 +37,62 @@ namespace CFG								// context-free grammar
 
 	// Backus-Naur form
 	class BNFGrammar {
-		std::map<std::string, LexemeID> id;						// map[lexeme] == LexemeID
-		std::vector<std::string> lex;							// vector[LexemeID] == lexeme
-		std::vector<LexemeType> typeList;						// lexeme type
-		std::vector<Production> prodList;						// productions
-		LexemeID next;											// next free ID
+		std::map<std::string, LexemeID> id;						// map[lexeme] -> LexemeID
+		std::vector<std::string> lex;							// vector[LexemeID] -> lexeme
+		std::vector<LexemeType> typeSet;						// vector[LexemeID] -> LexemeType, lexeme type
+		std::vector<First> firstSet;							// vector[LexemeID] -> First, FIRST sets
+		std::vector<Follow> followSet;							// vector[LexemeID] -> Follow, FOLLOW sets
+		std::vector<Production> prodSet;						// productions
+		LexemeID next;											// next free LexemeID, first ID == 0, last ID == next - 1
+		LexemeID idES;											// LexemeID of the empty string lexeme
+		LexemeID idEOF;											// LexemeID of the END OF FILE lexeme
+		// static
 		static std::string prefNT;								// NT prefix
 		static std::string prefDer;								// prefix of derived
 		static std::string suffNT;								// NT suffix
 		static std::string lexES;								// lexeme of an empty string
+		static std::string lexEOF;								// lexeme of END OF FILE
 		static std::string comment;
+
+	private:
+		// const members
+		void Print(std::ostream& os, const std::vector<LexemeID>& set) const;
+		bool CheckOriginalGrammar(std::ostream& error) const;
+		bool SetContainsOnlyTerminalsAndNonterminals(const Derived& derived) const;
+		bool SetContains(const std::vector<LexemeID>& set, LexemeID target) const;
+		First GetFirstSetForProduction(size_t productionIndex, size_t derivedIndex) const;
+		int Merge(std::vector<LexemeID>& receiver, const std::vector<LexemeID>& source) const;
+		int MergeExcept(std::vector<LexemeID>& receiver, const std::vector<LexemeID>& source, LexemeID exception) const;
+
+		// nonconst members
+		void AddProduction(const Production& production) { prodSet.push_back(production); }
+		LexemeID GetLexemeID(const std::string& lexeme);
+		void RemoveDirectAndIndirectLeftRecursion(std::ostream& error);
+		void RemoveRecursion(std::ostream& error);
+		void FindAllNonterminals(std::ostream& error);
+		void FindAllFirstSets(std::ostream& error);
+		void FindAllFollowSets(std::ostream& error);
+
 	public:
 		BNFGrammar()
-			: next{ 0 }
+			: next{ 0 }, idES{ 0 }, idEOF{ 0 }
 		{
 			Production::gram = this;
 		}
 
 		// const members
-		const std::string& GetPrefixNT() const { return prefNT; }
-		const std::string& GetPrefixDerived() const { return prefDer; }
-		size_t GetProductionSize() const { return prodList.size(); }
-		const Production& GetProduction(size_t index) const { return prodList[index]; }
-		bool CheckGrammar(std::ostream& error) const;
+		//const std::string& GetPrefixNT() const { return prefNT; }
+		//const std::string& GetPrefixDerived() const { return prefDer; }
+		//size_t GetProductionSize() const { return prodList.size(); }
+		//const Production& GetProduction(size_t index) const { return prodList[index]; }
 
 		// nonconst members
-		void SetPrefixes(const std::string& prefixNT, const std::string& prefixDerived)
-		{
-			prefNT = prefixNT;
-			prefDer = prefixDerived;
-		}
-		void AddProduction(const Production& production) { prodList.push_back(production); }
-		LexemeID GetLexemeID(const std::string& lexeme);
-		void RemoveDirectAndIndirectLeftRecursion(std::ostream& error);
-		void FindAllNonterminals(std::ostream& error);
-		void RemoveRecursion(std::ostream& error);
+		//void SetPrefixes(const std::string& prefixNT, const std::string& prefixDerived)
+		//{
+		//	prefNT = prefixNT;
+		//	prefDer = prefixDerived;
+		//}
+		void TransformGrammar(std::ostream& error);
 
 		// friends
 		friend struct Production;
@@ -84,6 +107,7 @@ namespace CFG								// context-free grammar
 	std::string BNFGrammar::prefDer;
 	std::string BNFGrammar::suffNT;
 	std::string BNFGrammar::lexES;
+	std::string BNFGrammar::lexEOF;
 	std::string BNFGrammar::comment;
 }
 

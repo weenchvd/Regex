6d057ca54cba7fbd0dc13cb901b1f1f62a1b6fe0
weenchvd <d88744@gmail.com>
2021-03-10 17:24:40 +0300

    The project structure has been changed. The source code has not changed

---
+++ Test/Regex/regexpr-test.cpp
@@ -0,0 +1,13 @@
+//#include "../../Source/Regex/regexpr.hpp"
+#include "gtest/gtest.h"
+
+namespace RegexTest
+{
+	TEST(TestSuite1, TestName1) {
+		ASSERT_EQ(1, 1);
+	}
+
+
+
+
+} // namespace RegexTest
